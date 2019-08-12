bfd26e95af441c7fd6c67540697ac7741f4bb9ac
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-12 14:34:49 -0600

    Seeded accounts data and made GET request for all accounts

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  db("accounts")
+    .then(results => {
+      res.status(200).json(results);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not get results" });
+    });
+});
+
+server.post("/", (req, res) => {});
+
+server.delete("/:id", (req, res) => {});
+
+server.put("/:id", (req, res) => {});
 
 module.exports = server;
